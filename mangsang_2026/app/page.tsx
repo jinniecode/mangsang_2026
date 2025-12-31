f86ce88058108d2be2b2434bc1ba47640b69a8e7
jinniecode <90439161+jinniecode@users.noreply.github.com>
2026-01-01 02:05:44 +0900

    Update Kakao2 page.tsx

--- mangsang_2026/app/page.tsx
+++ mangsang_2026/app/page.tsx
@@ -11,7 +11,7 @@ declare global {
 
 // --- 1. 데이터 정의 (모든 텍스트 포함) ---
 
-const KAKAO_JS_KEY = process.env.NEXT_PUBLIC_KAKAO_JS_KEY || "203ef9e6613c1e84a888fbf05b814fab";
+const KAKAO_JS_KEY = process.env.NEXT_PUBLIC_KAKAO_JS_KEY || "e83044bf41c6f1abb08e666366c3a43a";
 
 type CategoryId = 'muscle' | 'money' | 'love' | 'quit' | 'godlife';
 
